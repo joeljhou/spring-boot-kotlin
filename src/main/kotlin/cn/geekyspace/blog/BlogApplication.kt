a7190b41df483b0c9370d0eab0cf19a4d30a77e9
joeljhou <joeljhou336@gmail.com>
2025-09-24 13:44:30 +0800

    忽略文件

--- src/main/kotlin/cn/geekyspace/blog/BlogApplication.kt
+++ src/main/kotlin/cn/geekyspace/blog/BlogApplication.kt
@@ -1,4 +1,4 @@
-package com.example.blog
+package cn.geekyspace.blog
 
 import org.springframework.boot.Banner
 import org.springframework.boot.autoconfigure.SpringBootApplication
@@ -8,8 +8,8 @@ import org.springframework.boot.runApplication
 class BlogApplication
 
 fun main(args: Array<String>) {
-	runApplication<BlogApplication>(*args){
-		// 关闭 Banner，默认会在控制台打印一个 ASCII Banner（Spring Boot 标志）
-		setBannerMode(Banner.Mode.OFF)
-	}
+    runApplication<BlogApplication>(*args){
+        // 关闭 Banner，默认会在控制台打印一个 ASCII Banner（Spring Boot 标志）
+        setBannerMode(Banner.Mode.OFF)
+    }
 }
